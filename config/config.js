bef0e09b14e47d705997d0add670a3b71523be1e
Gui Santos <86501120+guio11221@users.noreply.github.com>
2023-03-11 20:40:47 -0400

    atualizado

--- config/config.js
+++ config/config.js
@@ -3,9 +3,9 @@ const dados = {
 
     host: 'localhost',
     port: 5432,
-    user: 'root',
-    password: 'ifms',
-    database: 'portal_noticias'
+    user: '',
+    password: '',
+    database: ''
 
 }
 
