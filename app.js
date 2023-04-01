aeef137b24356de2e1b6351dec5bd15b1d02ea22
Gui Santos <86501120+guio11221@users.noreply.github.com>
2023-03-31 21:42:03 -0400

    att

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-const app = require('./config/server.js');
-const { dataExpress }  = require('./config/config')
+const app = require("./config/server.js"); // importando a configuração do express.
+const { dataExpress } = require("./config/config"); // Algumas variaveis que eu posso usar em todo o codigo
 
 
 
 // iniciando o servidor na porta que foi definida..!!
-app.listen(dataExpress.PORT, () => console.log(`Servidor rodando na porta ${dataExpress.PORT}`));
+app.listen(dataExpress.PORT, () =>
+    console.log(`Servidor rodando na porta ${dataExpress.PORT}`)
+);
