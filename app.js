b575afaa8356cd2487463a91e1c5f6ef0e3688e1
guio11221 <sguii5147@gmail.com>
2023-04-16 14:54:56 -0400

    Mensagem de commit

--- app.js
+++ app.js
@@ -2,11 +2,30 @@ const app = require("./config/server.js"); // importando a configuração do exp
 const PORT = 3000
 
 /**
- * @author Guii Santos
- * @description Atividade de frameWork, do IFMS Campus Dourados.
+
+    Como dizia o filósofo Piton, que teve como discípulo nada
+    mais nada menos do que Karl Marx. Tudo na vida depende do 
+    quanto você quer comer alguém. Você trabalha pra comer alguém,
+    você estuda pra comer alguém, você usa droga pra comer alguém,
+    você malha, toma veneno, faz a desgraça toda pra comer alguém, 
+    você vai num show com a intenção de sair de lá pra comer alguém, 
+    você faz doação, você faz caridade, posta foto da caridade 
+    que você fez no Instagram, no face, pras mina olhar: ó... 
+    ele é caridoso, ele tem pensamento social, ah vou dar pra ele, 
+    PRA COMER ALGUÉM DESGRAÇA, tudo na vida tem a intenção de comer 
+    alguém, a quantidade de vontade que você quer ESFOLAR UMA BUCETA 
+    DESGRAÇA, então não venha com essa de que a... eu sou, eu sou 
+    beneficente, eu sou amigo da comunidade, você não é amigo de 
+    desgraça nenhuma, você quer comer alguém rapaz, você quer fuder 
+    pra comer alguém, você não estuda, pra ter um emprego bom pra 
+    juntar dinheiro pra falar: a... não sei o que, eu sou bem 
+    sucedido. VOCÊ ESTUDA PRA CHEGAR A DESGRAÇA, EU TENHO DINHEIRO, 
+    EU VOU FUDER PUTA, EU VOU COMER TODO MUNDO DESGRAÇA. Então a 
+    ideia é essa, façam tudo, FAÇAM TUDO NA INTENÇÃO DE ESFOLAR 
+    UMA BUCETA DESGRAAAAAAAAÇA.
  */
 
 
 
 // iniciando o servidor na porta que foi definida..!!
-app.listen(PORT, () =>  console.log(`Servidor rodando na porta %d`, PORT));
+app.listen(PORT, () => console.log(`Servidor rodando na porta %d`, PORT));
