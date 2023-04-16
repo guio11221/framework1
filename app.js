57c41b2d3f806bafea3b3fe1f72f8c6991c59051
guio11221 <sguii5147@gmail.com>
2023-04-16 14:57:48 -0400

    Mensagem de commit

--- app.js
+++ app.js
@@ -3,26 +3,13 @@ const PORT = 3000
 
 /**
 
-    Como dizia o filósofo Piton, que teve como discípulo nada
-    mais nada menos do que Karl Marx. Tudo na vida depende do 
-    quanto você quer comer alguém. Você trabalha pra comer alguém,
-    você estuda pra comer alguém, você usa droga pra comer alguém,
-    você malha, toma veneno, faz a desgraça toda pra comer alguém, 
-    você vai num show com a intenção de sair de lá pra comer alguém, 
-    você faz doação, você faz caridade, posta foto da caridade 
-    que você fez no Instagram, no face, pras mina olhar: ó... 
-    ele é caridoso, ele tem pensamento social, ah vou dar pra ele, 
-    PRA COMER ALGUÉM DESGRAÇA, tudo na vida tem a intenção de comer 
-    alguém, a quantidade de vontade que você quer ESFOLAR UMA BUCETA 
-    DESGRAÇA, então não venha com essa de que a... eu sou, eu sou 
-    beneficente, eu sou amigo da comunidade, você não é amigo de 
-    desgraça nenhuma, você quer comer alguém rapaz, você quer fuder 
-    pra comer alguém, você não estuda, pra ter um emprego bom pra 
-    juntar dinheiro pra falar: a... não sei o que, eu sou bem 
-    sucedido. VOCÊ ESTUDA PRA CHEGAR A DESGRAÇA, EU TENHO DINHEIRO, 
-    EU VOU FUDER PUTA, EU VOU COMER TODO MUNDO DESGRAÇA. Então a 
-    ideia é essa, façam tudo, FAÇAM TUDO NA INTENÇÃO DE ESFOLAR 
-    UMA BUCETA DESGRAAAAAAAAÇA.
+    @author {String} Como dizia o filósofo Piton, que teve como discípulo nada
+    @author {String} mais nada menos do que Karl Marx. Tudo na vida depende do 
+    @author {String} quanto você quer comer alguém. Você trabalha pra comer alguém,
+    @author {String} você estuda pra comer alguém, você usa droga pra comer alguém,
+    @author {String} você malha, toma veneno, faz a desgraça toda pra comer alguém, 
+    @author {String} você vai num show com a intenção de sair de lá pra comer alguém, 
+    @author {String} você faz doação, você faz caridade, posta foto da caridade...
  */
 
 
